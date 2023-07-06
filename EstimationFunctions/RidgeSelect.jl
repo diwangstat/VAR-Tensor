ea1e583dff4c0406bd90c866abd5e27c216bb32c
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2023-07-06 22:41:48 +0200

    Swap numerator and denominator to align with paper

--- EstimationFunctions/RidgeSelect.jl
+++ EstimationFunctions/RidgeSelect.jl
@@ -10,9 +10,9 @@ function RidgeRankSelect(est,c)
         S2 = svd(tenmat(est,2)).S;
         S3 = svd(tenmat(est,3)).S;
 
-        r1 = findmax((S1[1:(p[1]-1)].+c)./(S1[2:p[1]].+c))[2];
-        r2 = findmax((S2[1:(p[2]-1)].+c)./(S2[2:p[2]].+c))[2];
-        r3 = findmax((S3[1:(p[3]-1)].+c)./(S3[2:p[3]].+c))[2];
+        r1 = findmax((S1[2:p[1]].+c)./(S1[1:(p[1]-1)].+c))[2];
+        r2 = findmax((S2[2:p[2]].+c)./(S2[1:(p[2]-1)].+c))[2];
+        r3 = findmax((S3[2:p[3]].+c)./(S3[1:(p[3]-1)].+c))[2];
 
     end
     return(rank = [r1,r2,r3])
